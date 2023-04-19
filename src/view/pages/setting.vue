4565eb299d55eba04e66bb7b76eed26b06561071
Azad-Aihub <128484356+Azad-Aihub@users.noreply.github.com>
2023-04-19 19:55:21 +0800

    Update setting.vue

--- src/view/pages/setting.vue
+++ src/view/pages/setting.vue
@@ -15,7 +15,7 @@
               本站仅供<a href="https://t.zsxq.com/0cy1WzxXu" target="_blank">【不止GPT】星球成员</a>内部使用。<br>现已接入OpenAI的Models API、Completions API、Chat API、Audio API、Images API、Files API、Fine-tunes API等。<br>右侧填入API KEY后，“模型”处自动检索当前可用模型。<br>需要保存当前会话内容，请点击“创建会话”--“导出会话”。
             </span>
             <div>
-              <a href="http://www.azad.asia/" target="_blank">我的主页</a>---<a href="https://faka.azad.asia/" target="_blank">我的小店</a>
+              <a href="http://www.azad.asia/" target="_blank">我的主页</a>---<a href="https://faka.aihub.ren/" target="_blank">我的小店</a>
             </div>
           </div>
         </transition>
